d5c4f71235d58c22285f120a52058791b639cc29
Janina Porkka <janina.porkka@gmail.com>
2019-05-06 19:04:02 +0300

    Update koodi.py

--- koodi.py
+++ koodi.py
@@ -1,14 +1,12 @@
  # -*- coding: cp1252 -*-
 
-#print("Tämä on Python-ohjelma, ")
+#print("TÃ¤mÃ¤ on Python-ohjelma, ")
 #print("kirjoiettuna IDLE-editorilla.")
 #print("Jee!")
-#print("I can write Python :D")
 #arvo = 500
-#muuttuja = "My first python String :))"
 #print(arvo)
 #print(muuttuja)
-#v = "merkkijonosisältöä"
+#v = "merkkijonosisÃ¤ltÃ¶Ã¤"
 #print("Muuttujaan muuttuja on tallennettu", v)
 muuttuja = 24
 muuttuja2 = 1000
